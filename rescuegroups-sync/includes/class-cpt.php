2ae773879750866f577afd5f8847a361f664af9f
GideonDelta <39777756+GideonDelta@users.noreply.github.com>
2025-06-11 19:06:42 -0400

    Add species and breed taxonomies

--- rescuegroups-sync/includes/class-cpt.php
+++ rescuegroups-sync/includes/class-cpt.php
@@ -5,6 +5,7 @@ class CPT {
     public function __construct() {
         add_action( 'init', [ $this, 'register_cpt' ] );
         add_action( 'init', [ $this, 'register_meta' ] );
+        add_action( 'init', [ $this, 'register_taxonomies' ] );
     }
 
     public function register_cpt() {
@@ -38,5 +39,34 @@ class CPT {
 
         register_post_meta( 'adoptable_pet', '_rescue_sync_featured', $args );
         register_post_meta( 'adoptable_pet', '_rescue_sync_hidden', $args );
+    }
+
+    /**
+     * Register taxonomies used by the adoptable_pet post type.
+     */
+    public function register_taxonomies() {
+        $species_labels = [
+            'name'          => __( 'Species', 'rescuegroups-sync' ),
+            'singular_name' => __( 'Species', 'rescuegroups-sync' ),
+        ];
+        $species_args = [
+            'labels'       => $species_labels,
+            'public'       => true,
+            'hierarchical' => true,
+            'show_in_rest' => true,
+        ];
+        register_taxonomy( 'pet_species', 'adoptable_pet', $species_args );
+
+        $breed_labels = [
+            'name'          => __( 'Breeds', 'rescuegroups-sync' ),
+            'singular_name' => __( 'Breed', 'rescuegroups-sync' ),
+        ];
+        $breed_args = [
+            'labels'       => $breed_labels,
+            'public'       => true,
+            'hierarchical' => true,
+            'show_in_rest' => true,
+        ];
+        register_taxonomy( 'pet_breed', 'adoptable_pet', $breed_args );
     }
 }
