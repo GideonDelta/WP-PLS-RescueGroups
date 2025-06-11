f947f89ee2b5eeca1740f13f991e1d34fbbf5e8c
GideonDelta <39777756+GideonDelta@users.noreply.github.com>
2025-06-11 17:38:22 -0400

    Add API key setting

--- rescuegroups-sync/includes/class-utils.php
+++ rescuegroups-sync/includes/class-utils.php
@@ -5,4 +5,8 @@ class Utils {
     public static function get_option( $key, $default = '' ) {
         return get_option( 'rescue_sync_' . $key, $default );
     }
+
+    public static function get_api_key() {
+        return self::get_option( 'api_key' );
+    }
 }
